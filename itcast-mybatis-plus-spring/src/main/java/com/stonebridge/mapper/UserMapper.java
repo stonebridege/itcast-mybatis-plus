f71d632df0dae9441a50ec673a086d9ad31bbecf
stonebridge <stonebridge@njfu.edu.cn>
2021-11-26 19:26:33 +0800

    2.4.Spring + Mybatis + Mybatis Plus

---
+++ itcast-mybatis-plus-spring/src/main/java/com/stonebridge/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.stonebridge.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.stonebridge.pojo.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
